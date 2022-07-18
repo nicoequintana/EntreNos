42091cec471d1632f74305adbfd1d4f45c934fea
NicolasEQuintana <nicolasequintana@gmail.com>
2022-07-17 22:59:01 -0300

    correciones de pregame

--- js/start.js
+++ js/start.js
@@ -3,7 +3,6 @@ let btnStart = document.querySelector('#btnStart');
 let loader = document.querySelector('#loader');
 
 function startGame() {
-    console.log('entro funcion startGame')
     location.href = 'pregame.html';
 }
 
